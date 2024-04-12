66ae63cf1da3e01b335fe1dfe48cb389656e23cb
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-04-12 22:43:01 +0200

    Implement common log messages

---
+++ log_messages.py
@@ -0,0 +1,21 @@
+# Start and End rows
+START_ROW_ERROR = "Start row number is not provided"
+END_ROW_ERROR = "End row number is not provided"
+
+START_BIGGER_THAN_END = "Start row number is bigger than End row number"
+START_BIGGER_THAN_LINE_COUNT = "Start row is bigger than CSV total lines"
+END_BIGGER_THAN_LINE_COUNT = "End row is bigger than CSV total lines"
+
+
+# Column Numbers
+NAME_COLUMN_ERROR = "Name column number is not provided"
+PHONE_COLUMN_ERROR = "Phone column number is not provided"
+MESSAGE_COLUMN_ERROR = "Message column number is not provided"
+
+NAME_COLUMN_BIGGER = "Name column is bigger than CSV total columns"
+PHONE_COLUMN_BIGGER = "Phone column is bigger than CSV total columns"
+MESSAGE_COLUMN_BIGGER = "Message column is bigger than CSV total columns"
+
+# CSV File Path
+CSV_NOT_EXIST = "CSV file it not loaded"
+
