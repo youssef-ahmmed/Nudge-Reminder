a4e4d0660138419b965f745797a9c6c5f4e48ad3
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-04-14 00:35:14 +0200

    Refactor whatsapp wrapper module and use a selenium-based solution

--- core/whatsapp_sender.py
+++ core/whatsapp_sender.py
@@ -1,27 +1,12 @@
 from time import sleep
 
-import pywhatkit
-from pywhatkit.core.exceptions import CountryCodeException, InternetException, CallTimeException
-
-from controller.log_controller import LogController
+from controller.open_browser_button_controller import OpenBrowserButtonController
+from core.csv_parser import parse_csv_rows
 
 
-def send_multiple_instant_messages(data: list[list]):
-    print('received data', data)
-    for row in data:
-        print(row)
-        # try:
-        pywhatkit.sendwhatmsg_instantly(row[1], row[2], 15, True, 3)
-        sleep(1)
-        # except (CountryCodeException, InternetException, CallTimeException) as e:
-        #     LogController.get_instance().log_error(e)
-        # except Exception as e:
-        #     print(e)
+def send_multiple_instant_messages():
+    messenger = OpenBrowserButtonController.get_instance().get_whatsapp_message_instance()
 
-
-def send_multiple_messages_at_time(data: list[list]):
-    ...
-
-
-def send_one_message_at_time(phone_number: str, message: str, hour: int, minutes: int):
-    pywhatkit.sendwhatmsg(phone_number, message, hour, minutes)
+    for row in parse_csv_rows():
+        messenger.send_direct_message(row[1], row[2], saved=False)
+        sleep(3)
