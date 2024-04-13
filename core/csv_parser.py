b397afc76e4e38e66fb76c710733cf3fa114018b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-04-13 21:25:49 +0200

    Allow parsing method of csv file to be generator

--- core/csv_parser.py
+++ core/csv_parser.py
@@ -9,7 +9,7 @@ def count_lines_and_columns_in_csv():
     csv_file_path: str = PathLoaderController.get_instance().get_csv_file_path()
 
     with open(csv_file_path, 'r') as file:
-        line_count = sum(1 for _ in file)
+        line_count = sum(1 for _ in file) - 1
         file.seek(0)
 
         first_line = next(file)
@@ -18,7 +18,7 @@ def count_lines_and_columns_in_csv():
     return line_count, column_count
 
 
-def parse_csv_to_list_of_lists():
+def parse_csv_rows():
     csv_file_path: str = PathLoaderController.get_instance().get_csv_file_path()
     name, phone, msg = ColumnDataNumbersController.get_instance().get_column_data_numbers().values()
     start, end = StartEndRowsController.get_instance().get_start_end_rows_data()
@@ -26,13 +26,13 @@ def parse_csv_to_list_of_lists():
     start_row, end_row = int(start) - 1, int(end) - 1
     name_col, phone_col, msg_col = int(name) - 1, int(phone) - 1, int(msg) - 1
 
-    data = []
     with open(csv_file_path, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
-        for row in csv_reader:
-            phone_number = row[phone_col] if row[phone_col].startswith('+') else f"+{row[phone_col]}"
-            selected_row = [row[name_col], phone_number, row[msg_col]]
-            data.append(selected_row)
-
-    return data[start_row:end_row + 1]
+        for i, row in enumerate(csv_reader):
+            if start_row <= i <= end_row:
+                phone_number = row[phone_col] if row[phone_col].startswith('+') else f"+{row[phone_col]}"
+                selected_row = [row[name_col], phone_number, row[msg_col]]
+                yield selected_row
+            elif i > end_row:
+                break
